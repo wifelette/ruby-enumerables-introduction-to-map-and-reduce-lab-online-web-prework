5c282160a359bb92f6f4a68d2984b847876c8859
Leah Silber <leahsilber@gmail.com>
2019-07-23 06:00:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,10 +62,10 @@ def reduce_to_any_true(source_array)
 end
 
 def reduce_to_all_true(source_array)
-  n = 0
+  i = 0
   while n < source_array.length do
-    return false if !source_array[n]
-    n += 1
+    return false if !source_array[i]
+    i += 1
   end
   return true
 end
