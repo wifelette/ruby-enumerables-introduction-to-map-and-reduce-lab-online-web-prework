287136b1a1faf7692270440b0f2b7c2f4600b176
Leah Silber <leahsilber@gmail.com>
2019-07-23 05:50:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,5 +53,10 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_any_true(source_array)
-
+	i = 0;
+	while i < source_array.length do
+		sourceArray[i] = true;
+		i += 1
+	end
+	total
 end
